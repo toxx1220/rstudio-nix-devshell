f87c3d6d3fcbcf37c330d94e3de71ca1065cf601
toxx <txx1337@proton.me>
2026-03-17 21:50:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Get Started
+Required: Nix package manager
+
+If you use direnv, you can just run `direnv allow` in the folder once to auto load the shell on entry.
+
+Otherwise run 'nix develop' in the folder to get a shell with R/RStudio.
+
+From there:
+
+run `rstudio` to launch the RStudio GUI
+
+run `R` to enter the R repl
+
+run `Rscript example.R` to execute R scripts from the command line, or `Rscript --help` to see more options.
+
+# Add R packages
+Should be as simple as editing the flake.nix rpackages block.
+Search the package name in any nixpkgs search tool or via the command line.
+Run `direnv reload` or `nix develop`
+
+# Update R
+run `nix flake update` and reload the shell
---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "R and RStudio Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        rpkgs = with pkgs.rPackages; [
+          ggplot2
+          # Packages can be found under 'rPackages.*'
+          # e.g., rmarkdown, knitr, etc.
+        ];
+
+				# Link CLI/GUI with defined r packages
+        r-wrapper = pkgs.rWrapper.override { packages = rpkgs; };
+        r-studio-wrapper = pkgs.rstudioWrapper.override { packages = rpkgs; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            r-wrapper
+            r-studio-wrapper
+          ];
+
+          shellHook = ''
+						echo ""
+          	echo "-----------------------------------------------"
+          	echo "run \`rstudio\` to launch the RStudio GUI"
+						echo ""
+						echo "run \`R\` to enter the R repl"
+						echo ""
+          	echo "run \`Rscript example.R\` to execute R scripts from the command line, or \`Rscript --help\` to see more options."
+          	echo "-----------------------------------------------"
+						echo ""
+          '';
+        };
+      }
+    );
+}
